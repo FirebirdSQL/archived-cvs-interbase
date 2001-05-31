e9de8e4a2d141b79ebcf8a706c83b333b715bb5d
cvs2git <firebird@firebirdsql.org>
2001-05-31 03:00:20 +0000

    This commit was manufactured by cvs2git to create tag 'systrig'.

--- firebird/bellardo/darwin/checkmakefiles.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-if [ `cat $2/builds/original/.version_flag` != $1 ]; then
-	make ROOT="$2" BUILDTYPE="$1" SERVERTYPE="$3" updatemakefiles
-fi
--- firebird/fsg/TCS/fix_it.sql
+++
@@ -1,4 +0,0 @@
-set sql dialect 3;
-update tests set "DATE"='01.01.80' where "DATE"<'01.01.80';
-commit;
-exit;
