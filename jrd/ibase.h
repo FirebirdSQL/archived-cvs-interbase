697a069265f059a60d1f471b118340caf1ff9974
Ann W. Harrison <awharrison@users.sourceforge.net>
2001-09-22 16:05:04 +0000

    VS: Committing in . Give all enums explicit values.

--- jrd/ibase.h
+++ jrd/ibase.h
@@ -1864,79 +1864,83 @@ BSTREAM   ISC_FAR * ISC_EXPORT Bopen2();
 enum db_info_types
     {
 	isc_info_db_id = 4,
-	isc_info_reads,
-	isc_info_writes,
-	isc_info_fetches,
-	isc_info_marks,
+	isc_info_reads = 5,
+	isc_info_writes = 6,
+	isc_info_fetches = 7,
+	isc_info_marks = 8,
 
 	isc_info_implementation = 11,
-	isc_info_isc_version,
-	isc_info_base_level,
-	isc_info_page_size,
-	isc_info_num_buffers,
-	isc_info_limbo,
-	isc_info_current_memory,
-	isc_info_max_memory,
-	isc_info_window_turns,
-	isc_info_license,   /* 20 */
+	isc_info_isc_version = 12,
+	isc_info_base_level = 13,
+	isc_info_page_size = 14,
+	isc_info_num_buffers = 15,
+	isc_info_limbo = 16,
+	isc_info_current_memory = 17,
+	isc_info_max_memory = 18,
+	isc_info_window_turns = 19,
+	isc_info_license = 20,   
 
-	isc_info_allocation,
-	isc_info_attachment_id,
-	isc_info_read_seq_count,
-	isc_info_read_idx_count,
-	isc_info_insert_count,
-	isc_info_update_count,
-	isc_info_delete_count,
-	isc_info_backout_count,
-	isc_info_purge_count,
-	isc_info_expunge_count,   /* 30 */
+	isc_info_allocation = 21,
+	isc_info_attachment_id = 22,
+	isc_info_read_seq_count = 23,
+	isc_info_read_idx_count = 24,
+	isc_info_insert_count = 25,
+	isc_info_update_count = 26,
+	isc_info_delete_count = 27,
+	isc_info_backout_count = 28,
+	isc_info_purge_count = 29,
+	isc_info_expunge_count = 30, 
 
-	isc_info_sweep_interval,
-	isc_info_ods_version,
-	isc_info_ods_minor_version,
-	isc_info_no_reserve,
-	isc_info_logfile,
-	isc_info_cur_logfile_name,
-	isc_info_cur_log_part_offset,
-	isc_info_num_wal_buffers,
-	isc_info_wal_buffer_size,
-	isc_info_wal_ckpt_length,   /* 40 */
+	isc_info_sweep_interval = 31,
+	isc_info_ods_version = 32,
+	isc_info_ods_minor_version = 33,
+	isc_info_no_reserve = 34,
+	isc_info_logfile = 35,
+	isc_info_cur_logfile_name = 36,
+	isc_info_cur_log_part_offset = 37,
+	isc_info_num_wal_buffers = 38,
+	isc_info_wal_buffer_size = 39,
+	isc_info_wal_ckpt_length = 40,   
 
-	isc_info_wal_cur_ckpt_interval,  
-	isc_info_wal_prv_ckpt_fname,
-	isc_info_wal_prv_ckpt_poffset,
-	isc_info_wal_recv_ckpt_fname,
-	isc_info_wal_recv_ckpt_poffset,
+	isc_info_wal_cur_ckpt_interval = 41,  
+	isc_info_wal_prv_ckpt_fname = 42,
+	isc_info_wal_prv_ckpt_poffset = 43,
+	isc_info_wal_recv_ckpt_fname = 44,
+	isc_info_wal_recv_ckpt_poffset = 45,
 	isc_info_wal_grpc_wait_usecs = 47,
-	isc_info_wal_num_io,
-	isc_info_wal_avg_io_size,
-	isc_info_wal_num_commits,   /* 50 */
+	isc_info_wal_num_io = 48,
+	isc_info_wal_avg_io_size = 49,
+	isc_info_wal_num_commits = 50,  
 
-	isc_info_wal_avg_grpc_size,
-	isc_info_forced_writes,
-	isc_info_user_names,
-	isc_info_page_errors,
-	isc_info_record_errors,
-	isc_info_bpage_errors,
-	isc_info_dpage_errors,
-	isc_info_ipage_errors,
-	isc_info_ppage_errors,
-	isc_info_tpage_errors,
-	isc_info_set_page_buffers,
-	isc_info_db_sql_dialect,    /* 60 */
+	isc_info_wal_avg_grpc_size = 51,
+	isc_info_forced_writes = 52,
+	isc_info_user_names = 53,
+	isc_info_page_errors = 54,
+	isc_info_record_errors = 55,
+	isc_info_bpage_errors = 56,
+	isc_info_dpage_errors = 57,
+	isc_info_ipage_errors = 58,
+	isc_info_ppage_errors = 59,
+	isc_info_tpage_errors = 60,
 
-	isc_info_db_read_only,
-	isc_info_db_size_in_pages,
-	isc_info_oldest_transaction,
-	isc_info_oldest_active,
-	isc_info_oldest_snapshot,
-	isc_info_next_transaction,
-	isc_info_db_provider,
-	isc_info_db_class,
-	isc_info_firebird_version,
+	isc_info_set_page_buffers = 61,
+	isc_info_db_sql_dialect = 62,   
+	isc_info_db_read_only = 63,
+	isc_info_db_size_in_pages = 64,
 
-	frb_info_att_charset = 101
-	};
+    /* Values 65 -100 unused to avoid conflict with InterBase */
+	
+	frb_info_att_charset = 101,
+	isc_info_db_class = 102,
+	isc_info_firebird_version = 103,
+	isc_info_oldest_transaction = 104,
+	isc_info_oldest_active = 105,
+	isc_info_oldest_snapshot = 106,
+	isc_info_next_transaction = 107,
+	isc_info_db_provider = 108,
+
+	isc_info_db_last_value   // Leave this LAST!
+    };
 
 
 #define isc_info_version isc_info_isc_version
@@ -1948,57 +1952,59 @@ enum db_info_types
 
 enum  info_db_implementations
     {
-	isc_info_db_impl_rdb_vms  = 1,
-	isc_info_db_impl_rdb_eln,
-	isc_info_db_impl_rdb_eln_dev,
-	isc_info_db_impl_rdb_vms_y,
-	isc_info_db_impl_rdb_eln_y,
-	isc_info_db_impl_jri,
-	isc_info_db_impl_jsv,
+	isc_info_db_impl_rdb_vms = 1,
+	isc_info_db_impl_rdb_eln = 2,
+	isc_info_db_impl_rdb_eln_dev = 3,
+	isc_info_db_impl_rdb_vms_y = 4,
+	isc_info_db_impl_rdb_eln_y = 5,
+	isc_info_db_impl_jri = 6,
+	isc_info_db_impl_jsv = 7,
 
 	isc_info_db_impl_isc_apl_68K = 25,
-	isc_info_db_impl_isc_vax_ultr,
-	isc_info_db_impl_isc_vms,
-	isc_info_db_impl_isc_sun_68k,
-	isc_info_db_impl_isc_os2,
-	isc_info_db_impl_isc_sun4,	   /* 30 */
+	isc_info_db_impl_isc_vax_ultr = 26,
+	isc_info_db_impl_isc_vms = 27,
+	isc_info_db_impl_isc_sun_68k = 28,
+	isc_info_db_impl_isc_os2 = 29,
+	isc_info_db_impl_isc_sun4 = 30,	   /* 30 */
 	
-	isc_info_db_impl_isc_hp_ux,
-	isc_info_db_impl_isc_sun_386i,
-	isc_info_db_impl_isc_vms_orcl,
-	isc_info_db_impl_isc_mac_aux,
-	isc_info_db_impl_isc_rt_aix,
-	isc_info_db_impl_isc_mips_ult,
-	isc_info_db_impl_isc_xenix,
-	isc_info_db_impl_isc_dg,
-	isc_info_db_impl_isc_hp_mpexl,
-	isc_info_db_impl_isc_hp_ux68K,	  /* 40 */
+	isc_info_db_impl_isc_hp_ux = 31,
+	isc_info_db_impl_isc_sun_386i = 32,
+	isc_info_db_impl_isc_vms_orcl = 33,
+	isc_info_db_impl_isc_mac_aux = 34,
+	isc_info_db_impl_isc_rt_aix = 35,
+	isc_info_db_impl_isc_mips_ult = 36,
+	isc_info_db_impl_isc_xenix = 37,
+	isc_info_db_impl_isc_dg = 38,
+	isc_info_db_impl_isc_hp_mpexl = 39,
+	isc_info_db_impl_isc_hp_ux68K = 40,	  /* 40 */
 
-	isc_info_db_impl_isc_sgi,
-	isc_info_db_impl_isc_sco_unix,
-	isc_info_db_impl_isc_cray,
-	isc_info_db_impl_isc_imp,
-	isc_info_db_impl_isc_delta,
-	isc_info_db_impl_isc_next,
-	isc_info_db_impl_isc_dos,
-	isc_info_db_impl_m88K,		/* 48 */
-	isc_info_db_impl_unixware,	/* 49 */
-	isc_info_db_impl_isc_winnt_x86,	/* 50 */
+	isc_info_db_impl_isc_sgi = 41,
+	isc_info_db_impl_isc_sco_unix = 42,
+	isc_info_db_impl_isc_cray = 43,
+	isc_info_db_impl_isc_imp = 44,
+	isc_info_db_impl_isc_delta = 45,
+	isc_info_db_impl_isc_next = 46,
+	isc_info_db_impl_isc_dos = 47,
+	isc_info_db_impl_m88K = 48,
+	isc_info_db_impl_unixware = 49,
+	isc_info_db_impl_isc_winnt_x86 = 50,
 
-	isc_info_db_impl_isc_epson,	/* 51 */
-	isc_info_db_impl_alpha_osf,	/* 52 */
-	isc_info_db_impl_alpha_vms,	/* 53 */
-	isc_info_db_impl_netware_386,	/* 54 */ 
-	isc_info_db_impl_win_only,	/* 55 */
-	isc_info_db_impl_ncr_3000,	/* 56 */
-	isc_info_db_impl_winnt_ppc,	/* 57 */
-	isc_info_db_impl_dg_x86,	/* 58 */
-	isc_info_db_impl_sco_ev,	/* 59 */
-	isc_info_db_impl_i386,		/* 60 */
+	isc_info_db_impl_isc_epson = 51,
+	isc_info_db_impl_alpha_osf = 52,
+	isc_info_db_impl_alpha_vms = 53,
+	isc_info_db_impl_netware_386 = 54, 
+	isc_info_db_impl_win_only = 55,
+	isc_info_db_impl_ncr_3000 = 56,
+	isc_info_db_impl_winnt_ppc = 57,
+	isc_info_db_impl_dg_x86 = 58,
+	isc_info_db_impl_sco_ev = 59,
+	isc_info_db_impl_i386 = 60,
 
-	isc_info_db_impl_freebsd,	/* 61 */
-	isc_info_db_impl_netbsd,	/* 62 */
-	isc_info_db_impl_darwin		/* 63 */
+	isc_info_db_impl_freebsd = 61,
+	isc_info_db_impl_netbsd = 62,
+	isc_info_db_impl_darwin = 63,
+
+	isc_info_db_impl_last_value   // Leave this LAST!
     };
 
 #define isc_info_db_impl_isc_a            isc_info_db_impl_isc_apl_68K
@@ -2011,25 +2017,29 @@ enum  info_db_implementations
 enum	info_db_class
     {
 	isc_info_db_class_access = 1,
-	isc_info_db_class_y_valve,
-	isc_info_db_class_rem_int,
-	isc_info_db_class_rem_srvr,
+	isc_info_db_class_y_valve = 2,
+	isc_info_db_class_rem_int = 3,
+	isc_info_db_class_rem_srvr = 4,
 	isc_info_db_class_pipe_int = 7,
-	isc_info_db_class_pipe_srvr,
-	isc_info_db_class_sam_int,
-	isc_info_db_class_sam_srvr,
-	isc_info_db_class_gateway,
-	isc_info_db_class_cache,
-	isc_info_db_class_classic_access,
-	isc_info_db_class_server_access
+	isc_info_db_class_pipe_srvr = 8,
+	isc_info_db_class_sam_int = 9,
+	isc_info_db_class_sam_srvr = 10,
+	isc_info_db_class_gateway = 11,
+	isc_info_db_class_cache = 12,
+	isc_info_db_class_classic_access = 13,
+	isc_info_db_class_server_access = 14,
+
+	isc_info_db_class_last_value   // Leave this LAST!
     };
 
 enum	info_db_provider
     {
-	isc_info_db_code_rdb_eln,
-	isc_info_db_code_rdb_vms,
-	isc_info_db_code_interbase,
-	isc_info_db_code_firebird
+	isc_info_db_code_rdb_eln = 1,
+	isc_info_db_code_rdb_vms = 2,
+	isc_info_db_code_interbase = 3,
+	isc_info_db_code_firebird = 4,
+
+	isc_info_db_code_last_value   // Leave this LAST!
     };
 
 
