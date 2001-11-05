abf8c97b0c4879a51ef848c4caed705442c007c3
Paul Reeves <paul_reeves@users.sourceforge.net>
2001-11-05 09:46:17 +0000

    Added licence header to FB_Build_Win32_BSCMake.bat

--- FB_Build_Win32_BSCMake.bat
+++ FB_Build_Win32_BSCMake.bat
@@ -1,3 +1,20 @@
+:: The contents of this file are subject to the Independant
+:: Developers Public License Version 1.0 (the "License").
+:: You may not use this file except in compliance with the License.
+:: You may obtain a copy of the License at http://www.ibphoenix.com/IDPL.html
+:: 
+:: Software distributed under the License is distributed on an
+:: "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express
+:: or implied. See the License for the specific language governing
+:: rights and limitations under the License.
+
+:: The Original Code was created by Paul Reeves. Portions created
+:: by him are Copyright (C) Paul Reeves.
+:: 
+:: All Rights Reserved.
+:: Contributor(s): __________________________________.
+::
+
 
 ::After a debug build, this will take all the .sbr files and
 :: create a browse database.
